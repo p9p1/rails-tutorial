88538874740f4db5bbf26a908c3b707bc81c58a7
EC2 Default User <xwnz0gd@gmail.com>
2018-11-24 13:40:58 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-
+  resources :users
   # => contact_path
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
